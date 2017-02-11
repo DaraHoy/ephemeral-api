142f513d9f599ce3eb0098c0bb4e8d2eae12e001
DaraHoy <darahoy@gmail.com>
2017-02-11 15:00:22 -0500

    added cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // =============================================================================
 var express        = require('express');
 var app            = express();
+var cors           = require('cors');
 var bodyParser     = require('body-parser');
 var mongoose       = require("mongoose");
 var Entree         = require('./app/models/entree');
@@ -12,8 +13,9 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://dhoy:pleasant@ds145669.mlab.com:45669/ephemeral');
 
 
-// Configure app to use bodyParser()
+// Configure app to use bodyParser() & cors
 // allows access to data from POST
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
